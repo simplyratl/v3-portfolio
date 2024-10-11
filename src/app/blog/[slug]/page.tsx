df579de709f71087ec817585fea562dd3ccaebc0
simplyratl <nikicaraznatovic123@gmail.com>
2024-10-11 08:24:56 +0200

    changing style of toc

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -18,6 +18,15 @@ type Headings = {
   level: number;
 };
 
+const headingLevels: {
+  [key: string]: number;
+} = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+};
+
 async function getDocFromParams(slug: string) {
   const doc = allBlogs.find((doc) => doc.slugAsParams === slug);
   if (!doc) notFound();
@@ -50,17 +59,25 @@ export default async function BlogPage({ params }: BlogPageProps) {
       {blog.toc && (
         <aside>
           <ul
-            className="group/list fixed left-4 top-20 hidden max-h-[80vh] max-w-60 scroll-p-2.5 space-y-2 overflow-y-auto text-balance xl:block"
+            className="fixed left-4 top-20 hidden max-h-[80vh] max-w-60 scroll-p-2.5 space-y-2 overflow-y-auto text-balance text-sm opacity-40 transition-opacity hover:opacity-100 xl:block"
             id="toc"
           >
             {blog.headings.map((heading: Headings) => {
               return (
-                <li key={`#${heading.slug}`} className="group">
+                <li key={`#${heading.slug}`}>
                   <a
-                    className="!text-muted/40 group-hover/list:!text-inherit data-[level=three]:pl-4"
+                    className="group relative !text-muted/40 hover:!text-foreground data-[level=three]:pl-4"
                     data-level={heading.level}
                     href={`#${heading.slug}`}
+                    style={{
+                      paddingLeft: `${headingLevels[heading.level] * 9}px`,
+                    }}
                   >
+                    <span className="absolute left-0 top-0.5 font-mono text-xs">
+                      {new Array(headingLevels[heading.level])
+                        .fill(0)
+                        .map(() => "-")}
+                    </span>
                     {heading.text}
 
                     <span className="invisible ml-2 opacity-0 transition-all group-hover:visible group-hover:opacity-100">
