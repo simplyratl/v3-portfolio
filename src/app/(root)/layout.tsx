3b309f6d46557744a2fb35c3ccf0c02eefc21352
nikicaraznatovic <nikicaraznatovic123@gmail.com>
2024-10-13 11:16:41 +0200

    adding animation on background home page

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -8,8 +8,8 @@ export default function RootLayout({ children }: Props) {
   return (
     <div className="overflow-hidden">
       <div className="relative mx-auto max-w-[750px]">
-        <div className="absolute -left-10 -top-60 z-[-1] size-[30rem] rounded-full bg-gradient-to-br from-yellow-300 to-purple-800 opacity-20 blur-2xl dark:opacity-15"></div>
-        <div className="absolute -right-20 -top-72 z-[-1] size-[30rem] rounded-full bg-gradient-to-br from-red-100 to-violet-800 opacity-25 blur-2xl dark:opacity-15"></div>
+        <div className="absolute -left-10 -top-60 z-[-1] size-[30rem] animate-[spin_15s_linear_infinite] rounded-full bg-gradient-to-br from-yellow-300 to-purple-800 opacity-20 blur-2xl dark:opacity-15"></div>
+        <div className="absolute -right-20 -top-72 z-[-1] size-[30rem] animate-[spin_15s_linear_infinite] rounded-full bg-gradient-to-br from-red-100 to-violet-800 opacity-25 blur-2xl dark:opacity-15"></div>
       </div>
 
       <main className="mx-auto max-w-screen-md px-4 pt-8">{children}</main>
