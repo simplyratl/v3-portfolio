ca9c3d2dbdda0e999e005dc8ba699747954cdac8
nikicaraznatovic <nikicaraznatovic123@gmail.com>
2024-11-07 21:03:37 +0100

    update blogs ui

--- src/components/blogs/Blogs.tsx
+++ src/components/blogs/Blogs.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import ArrowUpRightIcon from "@/icons/ArrowUpIcon";
 import { allBlogs } from "contentlayer/generated";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -9,61 +8,33 @@ const getAllBlogs = () => {
   return allBlogs;
 };
 
-const blogsByYear = () => {
-  const blogs = getAllBlogs();
-  const blogsByYear: {
-    [key: string]: (typeof blogs)[0][];
-  } = {};
-  blogs.forEach((blog) => {
-    const year = new Date(blog.date).getFullYear();
-    if (!blogsByYear[year]) {
-      blogsByYear[year] = [];
-    }
-
-    blogsByYear[year].push(blog);
-  });
-  return Object.entries(blogsByYear).sort((a, b) => +b[0] - +a[0]);
-};
-
 export default function Blogs() {
-  const blogs = blogsByYear();
+  const blogs = getAllBlogs();
   const router = useRouter();
 
   return (
-    <div
-      className="slide-enter-content grid grid-cols-1 gap-2 space-y-8"
-      id="tab-projects"
-    >
-      {blogs.map(([year, blogs]) => (
-        <div key={year}>
-          <h1 className="text-2xl font-semibold text-muted/50 underline decoration-dashed">
-            {year}
-          </h1>
-          {blogs.map((blog, index) => (
-            <article
-              key={blog.title}
-              className="mt-4 text-pretty dark:text-foreground"
-              onMouseEnter={() => router.prefetch(blog.slug)}
+    <div className="slide-enter-content grid grid-cols-1 gap-2 space-y-8">
+      <ul>
+        {blogs.map((blog) => (
+          <li key={blog.slug} onMouseEnter={() => router.prefetch(blog.slug)}>
+            <Link
+              href={blog.slug}
+              className="group flex w-full items-center gap-2 rounded-lg px-4 py-3 no-underline transition-colors hover:bg-muted/5"
             >
-              {/*TODO: Da li koristiti link preview za blogove?*/}
-              {/*<LinkPreview*/}
-              {/*  url={`${blog.slug}`}*/}
-              {/*  className="inline-flex items-center gap-1"*/}
-              {/*>*/}
-              {/*</LinkPreview>*/}
-              <Link
-                href={blog.slug}
-                className="inline-flex items-center gap-1"
-                prefetch={index < 3}
-              >
-                <h2 className="text-md font-semibold">{blog.title}</h2>
-                <ArrowUpRightIcon className="h-4 w-4" />
-              </Link>
-              <p>{blog.description}</p>
-            </article>
-          ))}
-        </div>
-      ))}
+              <span className="font-semibold transition-all group-hover:underline">
+                {blog.title}
+              </span>
+              <span className="hidden text-sm text-muted/50 md:block">
+                {blog.description}
+              </span>
+              <div className="mr-2 h-[1px] flex-1 bg-muted/20"></div>
+              <span className="text-muted/60">
+                {new Date(blog.date).getFullYear()}
+              </span>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
